59d77ffdc2d095fd931a66b4a995aedd81b8dbf9
Jon Tiller <jon.w.tiller@gmail.com>
2021-11-25 21:34:54 +0100

    fix mixup naming of test

--- Tests/GenderTests.cs
+++ Tests/GenderTests.cs
@@ -12,14 +12,14 @@ namespace Tests
     public class GenderTests
     {
         [Test]
-        public void EvenDigit_Should_Return_Male()
+        public void OddDigit_Should_Return_Male()
         {
             var ssn = new Personnummer("03083626762");
             Assert.AreEqual(Gender.Male,ssn.Gender);
         }
 
         [Test]
-        public void OddDigit_Should_Return_Female()
+        public void EvenDigit_Should_Return_Female()
         {
             var ssn = new Personnummer("08065838215");
             Assert.AreEqual(Gender.Female, ssn.Gender);
